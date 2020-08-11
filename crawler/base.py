cb6619acc7d4424bec61e09f84dd7b8e9c05047e
root <780357902@qq.com>
2020-08-11 17:45:32 +0800

    单线程爬虫

--- crawler/base.py
+++ crawler/base.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import os
-from fake_useragent import UserAgent
-from urllib.request import urlretrieve
+import time
 
 os.chdir('/Users/zhengzhiheng/PycharmProjects/untitled3')
 download_path = './douban'
@@ -21,19 +20,23 @@ def download_pic(url):
 	images = content.find_all('img')  # 获取所有图片的标签
 	pic_link_list = [image['src'] for image in images]  # 所有图片的下载链接
 	pic_name_list = [image['alt'] for image in images]  # 所有电影名称
-	for name, link in zip(pic_name_list, pic_link_list): # 下载内容到本地
+	for name, link in zip(pic_name_list, pic_link_list):  # 下载内容到本地
 		html = requests.get(link)
 		with open(f'{download_path}/{name}.jpg', 'wb') as f:
 			f.write(html.content)
 
+
 def main():
 	start_urls = [base_target]
-
 	for i in range(1, 10):
 		start_urls.append(f'{base_target}?start={25 * i}&filter=')
+
+	start_time = time.time()
 	for url in start_urls:
 		download_pic(url)
-	print(start_urls)
+	end_time = time.time()
+	print('-' * 50)
+	print(f'运行时间：{end_time - start_time}')
 
 
 if __name__ == '__main__':
