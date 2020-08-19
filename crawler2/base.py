dd2eea6520a87e30a499f06f1386367cd412940b
root <780357902@qq.com>
2020-08-19 16:35:39 +0800

    使用selenium爬取github

---
+++ crawler2/base.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import requests
+
+CHROMEDRIVER = '/Users/zhengzhiheng/PycharmProjects/untitled3/myproject/crawler2/chromedriver'
+
+# 1. 创建浏览器对象
+# driver = webdriver.Chrome(executable_path=CHROMEDRIVER)
+# 2. 发送请求
+# driver.get('https://www.baidu.com')
+
+# 模拟搜索操作
+# input = driver.find_element_by_id('kw')
+# input.clear()
+# input.send_keys('CSDN')
+# input.send_keys(Keys.ENTER)
+
+name = 'ant-design/ant-design-pro'
+
+api = 'https://api.github.com/repos/' + name
+weburl = 'https://github.com/' + name
+
+old_time = None
+while True:
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+	r = requests.get(api, headers=headers)
+	if r.status_code != 200:
+		print('请求API失败')
+		break
+	'''
+		判断项目是否更新
+	'''
+	# json -> dict
+	now_time = r.json()['updated_at']
+	if not old_time:
+		old_time = now_time
+	if old_time < now_time:
+		print('项目更新了')
+		driver = webdriver.Chrome(executable_path=CHROMEDRIVER)
+		driver.get(weburl)
+	time.sleep(600)
